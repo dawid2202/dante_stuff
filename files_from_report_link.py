cb50326346b80da4f115bee1519b01c876b90d97
dawid2202 <92376428+dawid2202@users.noreply.github.com>
2024-01-17 21:58:13 +0100

    download dante project (for local testing)

---
+++ files_from_report_link.py
@@ -0,0 +1,27 @@
+import requests
+import re
+import os
+from zipfile import ZipFile
+
+url = input("gimme link: ")
+
+path="files/"
+
+dante = requests.get(url)
+
+danteFiles = re.findall('Pobierz wygenerowany plik: <a href="(.*)">', dante.text)
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
+zip_files = requests.get(url.replace('index.html', 'source.zip'))
+file = open(path + '/source.zip', 'wb')
+file.write(zip_files.content)
+file.close()
+ZipFile(path + '/source.zip').extractall(path=path)
+
+for file in danteFiles:
+    fileToSave = requests.get(url.replace('index.html', file))
+    f = open(path+file[4:], "wb")
+    f.write(fileToSave.content)
+    f.close()
